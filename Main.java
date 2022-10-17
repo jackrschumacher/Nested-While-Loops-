ad4dae894948bdb6824cb757cb6b4b91419032ab
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-10-17 15:53:59 +0000

    Setup Additional Variables

--- Main.java
+++ Main.java
@@ -8,7 +8,11 @@ class Main {
     
     System.out.println("What is the annual intrest rate on this loan? (as a decimal -> i.e. 9.3% would be .093");
     double intrestRate = input.nextDouble();
-      
+
+    System.out.println("How many months would you like to have to pay this loan off?");
+    int numMonths = input.nextInt();
+
+    
     
   }
 }
