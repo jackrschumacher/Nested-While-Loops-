3100f1d905ecef58271cab07f55ea42f733956a1
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-10-17 16:07:39 +0000

    Finish If Statement and Fix Bugs

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class Main {
     Scanner input = new Scanner(System.in);
     
     System.out.println("=====Loan Calculator=====");
+    System.out.println("What is the loan amount?");
     double initialBalance = input.nextDouble();
     
     System.out.println("What is the annual intrest rate on this loan? (as a decimal -> i.e. 9.3% would be .093");
@@ -26,7 +27,16 @@ class Main {
       currentBalance *= Math.pow(1+intrestRate, 1.0/12);
       currentBalance -= paymentToTry;
     }
-    if(Math.abs)
+    if(Math.abs(currentBalance) > marginOfError){
+      if(currentBalance < 0){
+        maxPayment = paymentToTry;
+        paymentToTry = (maxPayment + minPayment) / 2;
+      }
+      else{
+        minPayment = paymentToTry;
+        paymentToTry = (maxPayment + minPayment) / 2;
+      }
+    }
     
   }
 }
