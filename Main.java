7ef0caf9790477dcee17d5b1a7c158e26d24e678
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-10-17 15:46:43 +0000

    Setup variables

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import java.util.scanner
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner input = new Scanner(System.in);
+    
+    system.out.println("=====Loan Calculator=====");
+    double initialBalance = input.nextDouble();
+    
+    System.out.println("What is the annual intrest rate on this loan? (as a decimal -> i.e. 9.3% would be .093");
+    double intrestRate = input.nextDouble();
+      
+    
   }
 }
