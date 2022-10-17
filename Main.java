fb30f019a18e876b5f549ec12ff2d2e688e76c5d
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-10-17 16:04:34 +0000

    Fix Bugs

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
-import java.util.scanner
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     
-    system.out.println("=====Loan Calculator=====");
+    System.out.println("=====Loan Calculator=====");
     double initialBalance = input.nextDouble();
     
     System.out.println("What is the annual intrest rate on this loan? (as a decimal -> i.e. 9.3% would be .093");
@@ -12,7 +12,21 @@ class Main {
     System.out.println("How many months would you like to have to pay this loan off?");
     int numMonths = input.nextInt();
 
+
+    double maxPayment = initialBalance;
+    double minPayment = 0;
+    double paymentToTry = initialBalance;
+    double marginOfError = 0.01; //one penny off
+    double currentBalance = initialBalance;
     
+    do{
+      
+    }while(Math.abs(currentBalance) > marginOfError);
+    for(int i = 0; i < numMonths; i++){
+      currentBalance *= Math.pow(1+intrestRate, 1.0/12);
+      currentBalance -= paymentToTry;
+    }
+    if(Math.abs)
     
   }
 }
